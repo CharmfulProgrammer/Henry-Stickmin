67814fb12f7d7073f278d5e852461865f74428f3
Najeeb Farhan <najeebfarhan6345@gmail.com>
2022-07-22 17:23:08 +0530

    need to do work on equation.py

--- henry_discord_bot/extensions/maths/equation.py
+++ henry_discord_bot/extensions/maths/equation.py
@@ -4,11 +4,22 @@ import requests
 
 plugin = lightbulb.Plugin("Equation")
 
+base_url = "https://x-math.herokuapp.com/api/"
+
 @plugin.command()
-@lightbulb.command("equation", "Gives a math equation for you to solve")
+@lightbulb.command("equation", "Gives a math equation for you to solve", aliases=["eq"])
 @lightbulb.implements(lightbulb.PrefixCommand)
 async def equation(ctx: lightbulb.Context):
-    pass
+    # for now, it'll be updated
+    res = requests.get(base_url + "random")
+    data = res.json()
+
+    expression = data["expression"]
+    answer = data["answer"]
+
+    await ctx.respond(expression)
+
+
 
 
 def load(bot: lightbulb.BotApp):
